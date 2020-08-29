9d4db831b694050a4224677969600e6813216165
abraham@hubtel.com <abraham@hubtel.com>
2020-08-29 22:26:50 +0000

    DI and clean service registration

--- Tweetbook/Startup.cs
+++ Tweetbook/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Tweetbook.Configs;
+using Tweetbook.Installers;
 
 namespace Tweetbook
 {
@@ -29,20 +30,7 @@ namespace Tweetbook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<DataContext>();
-
-            services.AddSwaggerGen(x =>
-            {
-                x.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Tweetbook API",
-                    Version = "v1"
-                });
-            });
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,14 +45,11 @@ namespace Tweetbook
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+
             var swaggerConfigurations = new SwaggerConfigurations();
             Configuration.GetSection(nameof(SwaggerConfigurations)).Bind(swaggerConfigurations);
 
-            app.UseSwagger(option =>
-            {
-                option.RouteTemplate = swaggerConfigurations.JsonRoute;
-            });
+            app.UseSwagger(option => { option.RouteTemplate = swaggerConfigurations.JsonRoute; });
 
             app.UseSwaggerUI(option =>
             {
